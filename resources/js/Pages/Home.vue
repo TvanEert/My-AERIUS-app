8a13f827098e3156e6643338e686c83b0034d443
TvanEert <99051246@mydavinci.nl>
2022-11-17 15:56:53 +0100

    Updated button and styling on dashboard

--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,15 +1,11 @@
 <script setup>
 import Map from '@/Components/Map.vue';
-import Buttons from '@/Components/LocationButtons.vue';
+import SituationButton from '@/Components/SituationButton.vue';
 </script>
 
 <template>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Map />
-                    <Buttons />
-                </div>
-            </div>
-        </div>
+    <div>
+        <Map/>
+        <SituationButton/>
+    </div>
 </template>
