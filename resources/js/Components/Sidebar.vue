9832f6827e4e5144895255e3aa07004aed6369b1
TvanEert <99051246@mydavinci.nl>
2022-11-17 19:55:47 +0100

    Created basic sidebar

---
+++ resources/js/Components/Sidebar.vue
@@ -0,0 +1,35 @@
+<script>
+export default{
+    methods: {
+        toggleMenu(){
+            document.getElementById('nav-icon').classList.toggle('open')
+            document.getElementById('sidebar').classList.toggle('translate-x-[-100%]')
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="flex w-full bg-aerius-darkblue">
+        <div @click="toggleMenu()" id="nav-icon">
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+        </div>
+    </div>
+    <div class="flex absolute pt-16 pl-8 top-0 h-full w-[80%] bg-aerius-lightblue ease-in duration-300 translate-x-[-100%]" id="sidebar">
+        <ul>
+            <li class="font-bold text-white text-2xl mb-5">
+                <a :href="route('test2')">
+                    Recente berekeningen
+                </a>
+            </li>
+            <li class="font-bold text-white text-2xl mb-5">
+                <a :href="route('test2')">
+                    Instellingen
+                </a>
+            </li>
+        </ul>
+    </div>
+</template>
--- resources/js/Pages/Home.vue
+++ resources/js/Pages/Home.vue
@@ -1,15 +1,16 @@
 <script setup>
 import Map from '@/Components/Map.vue';
 import Buttons from '@/Components/LocationButtons.vue';
+import Sidebar from '@/Components/Sidebar.vue';
+
+import '@/../css/stylesheet.css';
 </script>
 
+
 <template>
-        <div class="py-12">
-            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
-                    <Map />
-                    <Buttons />
-                </div>
-            </div>
-        </div>
+    <div class="">
+        <Sidebar/>
+        <Map/>
+        <Buttons/>
+    </div>
 </template>
