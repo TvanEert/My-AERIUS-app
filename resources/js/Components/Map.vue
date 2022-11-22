f4d1057e8728201c166277bf986d86586a6c9244
RvanHulst <99051006@mydavinci.nl>
2022-11-22 11:51:36 +0100

    Created map component
    Installed OpenLayers package
    Created map component
    Imported OpenLayers package

--- resources/js/Components/Map.vue
+++ resources/js/Components/Map.vue
@@ -1,5 +1,40 @@
 <template>
-    <div class="flex place-content-center my-2 border-1">
-        <img src="https://upload.wikimedia.org/wikipedia/en/f/f8/Google_Maps_Android_11.png" width="350">
-    </div>
-</template>
+    <div ref="map-root" class="w-60 h-60"></div>
+  </template>
+  
+  <script>
+    import View from 'ol/View'
+    import Map from 'ol/Map'
+    import TileLayer from 'ol/layer/Tile'
+    import OSM from 'ol/source/OSM'
+  
+    // importing the OpenLayers stylesheet is required for having
+    // good looking buttons!
+    import 'ol/ol.css'
+  
+    export default {
+      name: 'Map',
+      components: {},
+      props: {},
+      mounted() {
+        // this is where we create the OpenLayers map
+        new Map({
+          // the map will be created using the 'map-root' ref
+          target: this.$refs['map-root'],
+          layers: [
+            // adding a background tiled layer
+            new TileLayer({
+              source: new OSM() // tiles are served by OpenStreetMap
+            }),
+          ],
+  
+          // the map view will initially show the whole world
+          view: new View({
+            zoom: 0,
+            center: [0, 0],
+            constrainResolution: true
+          }),
+        })
+      },
+    }
+  </script>
